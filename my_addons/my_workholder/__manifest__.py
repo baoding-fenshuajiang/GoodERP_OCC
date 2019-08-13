d520cf37f1241ac71889039cb9fa733aacef404d
baoding-fenshuajiang <openerp@126.com>
2019-08-14 07:17:47 +0800

    Chang Workholder from product.template to a new model.
    Signed-off-by: baoding-fenshuajiang <openerp@126.com>

---
+++ my_addons/my_workholder/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': '夹具',
+    'version': '1.1',
+    'summary': '记录夹具的名称、图号、内部编号、存放位置、夹具制作人、夹具寿命',
+    'description': 'E-Mail:992102498@qq.com',
+    'author':  '保定-粉刷匠',
+    'website': 'http://blog.sina.com.cn/kaiyuanlvzhou',
+    'depends': [],
+    'sequence': 13,
+    'demo': [
+
+       ],
+    'data': [
+        'views/my_workholder_views.xml'
+    ],
+    'qweb': [
+
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ my_addons/my_workholder/models/my_workholder.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+from odoo import api, fields, models
+from odoo.osv import expression
+
+
+# 在产品模型中增加图号、位置、制作人、使用寿命字段
+class MyWorkholder(models.Model):
+    _name = "equipment.workholder"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "夹具"
+    _order = "name"
+    name = fields.Char('Name', index=True, required=True, translate=False)
+    drawing_number = fields.Char(string=u'图号')
+    location = fields.Char(string=u'位置')
+    producer = fields.Many2one('hr.employee', string=u'制作人')
+    durability =  fields.Integer(string=u'使用寿命(件)')
+    description = fields.Text('备注', translate=False)
+    default_code = fields.Char('编号', index=True)
+    active = fields.Boolean('有效', default=True,help="如果取消勾选，可以实现隐藏这个产品而不是移除它。")
+
+    @api.multi
+    def name_get(self):
+        return [(template.id, '%s%s%s' % (template.default_code and '[%s] ' % template.default_code or '', template.name
+                                          , template.drawing_number and '[%s]' % template.drawing_number))
+                for template in self]
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = args or []
+        domain = []
+        if name:
+            domain = ['|','|', ('default_code',  operator, name), ('name', operator, name),
+                      ('drawing_number', operator, name)]
+            if operator in expression.NEGATIVE_TERM_OPERATORS:
+                domain = ['&', '!'] + domain[1:]
+        product_ids = self._search(domain + args, limit=limit, access_rights_uid=name_get_uid)
+        return self.browse(product_ids).name_get()
+
